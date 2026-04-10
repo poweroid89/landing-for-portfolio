20c58c9d0c0ec3a6ef0c34e9b04a9cfaafc9c3dd
Roman <poweroid89@gmail.com>
2026-04-10 11:41:28 +0300

    Small fix, remove no needed file

--- theme/functions.php
+++ theme/functions.php
@@ -16,7 +16,6 @@ define( 'LANDING_THEME_URI', get_template_directory_uri() );
 require_once LANDING_THEME_DIR . '/inc/theme-setup.php';
 require_once LANDING_THEME_DIR . '/inc/enqueue.php';
 require_once LANDING_THEME_DIR . '/inc/register-blocks.php';
-require_once LANDING_THEME_DIR . '/inc/enqueue-assets.php'; // Lazy loading logic
 require_once LANDING_THEME_DIR . '/inc/acf-options.php';     // ACF Options page
 
 /**
